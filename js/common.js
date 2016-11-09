482715e75149a2eb675ddd6cfc770764d0399461
Max <flexagon89@gmail.com>
2016-11-09 17:35:37 +0200

    pravki2

--- js/common.js
+++ js/common.js
@@ -28,16 +28,30 @@
             fade: true,
             cssEase: 'linear'
         });
-        
+        $('.slideClickLeft').on('click', function (event) {
+            var indexElem = $(this).index();
+            var indexSlide = $('.slideClick').index();
+
+            $('.slideClickLeft').removeClass('activeSlide2');
+            $(this).addClass('activeSlide2');
+            $('.slider2').find('.slick-dots').children('li').eq(indexElem).trigger( "click" );
+        });
+
+        $('.slider2').find('.slick-next').on('click', function (event) {
+            resetClassActiveSlider2();
+        });
+        $('.slider2').find('.slick-prev').on('click', function (event) {
+            resetClassActiveSlider2();
+        });
+        function resetClassActiveSlider2() {
+            setTimeout(function () {
+                $('.slideClickLeft').removeClass('activeSlide2');
+                //console.log($('.slider2').find('.slick-track').children('.slick-active').children('div').css('background-image'));
+                $('.slideClickLeft').eq($('.slider2').find('.slick-track').children('.slick-active').attr('data-slick-index')).addClass('activeSlide2');
+            },500);
+        };
 
     });
 
-    $('.slideClickLeft').on('click', function (event) {
-        var indexElem = $(this).index();
-        var indexSlide = $('.slideClick').index();
-        var dotsActive = $('.slick-dots').find('.slick-active').index();
-        console.log(dotsActive)
-        $('.slick-dots').children('li').eq(indexElem).trigger( "click" );
 
-    });
 })();
